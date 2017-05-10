9ff44ddebbedef540b5dc1de02028620f60b04fb
Stefan Lobato <code.lobato@gmail.com>
2017-05-10 20:22:49 +0200

    Created method to generate filled Integer Array

--- src/com/hva/Main.java
+++ src/com/hva/Main.java
@@ -2,7 +2,32 @@ package com.hva;
 
 public class Main {
 
+
     public static void main(String[] args) {
-	// write your code here
+        //  Create test Array Object
+        Integer[] testArray;
+
+        //  Fill test Array Object
+        testArray = GenerateArray(500);
+
+
+    }
+
+    /**
+     * Generate an Array with random Integers
+     *
+     * @param amountOfIndexes
+     * @return Generated Array
+     */
+    private static Integer[] GenerateArray(int amountOfIndexes) {
+        //  Generate Integer[] with given amount of indexes
+        Integer[] numbers = new Integer[amountOfIndexes];
+
+        for (int i = 0; i < numbers.length; i++) {
+            //  Fill index with a random number till the range of 1000 (can be Integer.MAX_VALUE)
+            numbers[i] = (int)(Math.random() * 1000);
+        }
+
+        return numbers;
     }
 }
