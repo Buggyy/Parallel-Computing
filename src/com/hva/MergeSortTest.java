2d871909415fc1ea40c78358b8b212fde73f1d85
Stefan Lobato <code.lobato@gmail.com>
2018-04-10 15:09:46 +0200

    Add MergeSort and MergeSortTest classes

---
+++ src/com/hva/MergeSortTest.java
@@ -0,0 +1,85 @@
+package com.hva;
+
+import org.junit.Before;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+/**
+ * Created by R. Lobato on 4/10/18.
+ */
+public class MergeSortTest {
+
+    private final static int SIZE = 7;
+    private final static int MAX = 20;
+    private int[] numbers;
+
+    @Before
+    public void setUp() throws Exception {
+        numbers = new int[SIZE];
+        Random generator = new Random();
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = generator.nextInt(MAX);
+        }
+    }
+
+    @org.testng.annotations.Test
+    public void sort() {
+        long startTime = System.currentTimeMillis();
+
+        MergeSort sorter = new MergeSort();
+        sorter.sort(numbers);
+
+        long stopTime = System.currentTimeMillis();
+        long elapsedTime = stopTime - startTime;
+        System.out.println("Mergesort " + elapsedTime);
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            if (numbers[i] > numbers[i + 1]) {
+                fail("Should not happen");
+            }
+        }
+        assertTrue(true);
+    }
+
+
+    @org.testng.annotations.Test
+    public void itWorksRepeatably() {
+        for (int i = 0; i < 200; i++) {
+            numbers = new int[SIZE];
+            Random generator = new Random();
+            for (int a = 0; a < numbers.length; a++) {
+                numbers[a] = generator.nextInt(MAX);
+            }
+            MergeSort sorter = new MergeSort();
+            sorter.sort(numbers);
+            for (int j = 0; j < numbers.length - 1; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    fail("Should not happen");
+                }
+            }
+            assertTrue(true);
+        }
+    }
+
+    @org.testng.annotations.Test
+    public void testStandardSort() {
+        long startTime = System.currentTimeMillis();
+        Arrays.sort(numbers);
+        long stopTime = System.currentTimeMillis();
+        long elapsedTime = stopTime - startTime;
+        System.out.println("Standard Java sort " + elapsedTime);
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            if (numbers[i] > numbers[i + 1]) {
+                fail("Should not happen");
+            }
+        }
+        assertTrue(true);
+    }
+
+
+}
